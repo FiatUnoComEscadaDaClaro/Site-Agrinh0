7738d36d3944668fda0c8530d32a824050b71ec0
FiatUnoComEscadaDaClaro <127310216+FiatUnoComEscadaDaClaro@users.noreply.github.com>
2024-08-08 19:01:21 -0300

    Delete js/game-over.js

--- js/game-over.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-  const finalTimeElement = document.getElementById('final-time');
-
-  // Obter o tempo final da localStorage
-  const finalTime = localStorage.getItem('finalTime') || '0';
-
-  finalTimeElement.textContent = finalTime;
-}
