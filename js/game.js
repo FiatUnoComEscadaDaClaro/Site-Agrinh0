f010360d5503ac714a3a47ccb057bc183ebd02b7
FiatUnoComEscadaDaClaro <127310216+FiatUnoComEscadaDaClaro@users.noreply.github.com>
2024-08-08 15:38:11 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -38,7 +38,6 @@ const checkCards = () => {
   const secondCharacter = secondCard.getAttribute('data-character');
 
   if (firstCharacter === secondCharacter) {
-
     firstCard.firstChild.classList.add('disabled-card');
     secondCard.firstChild.classList.add('disabled-card');
 
@@ -46,62 +45,52 @@ const checkCards = () => {
     secondCard = '';
 
     checkEndGame();
-
   } else {
     setTimeout(() => {
-
       firstCard.classList.remove('reveal-card');
       secondCard.classList.remove('reveal-card');
 
       firstCard = '';
       secondCard = '';
-
     }, 500);
   }
-
 }
 
 const revealCard = ({ target }) => {
-
   if (target.parentNode.className.includes('reveal-card')) {
     return;
   }
 
   if (firstCard === '') {
-
     target.parentNode.classList.add('reveal-card');
     firstCard = target.parentNode;
-
   } else if (secondCard === '') {
-
     target.parentNode.classList.add('reveal-card');
     secondCard = target.parentNode;
 
     checkCards();
-
   }
 }
 
 const createCard = (character) => {
-
   const card = createElement('div', 'card');
   const front = createElement('div', 'face front');
   const back = createElement('div', 'face back');
 
-  front.style.backgroundImage = `url('../imagens/${character}.png')`;
+  // Atualize o caminho para imagens na raiz do projeto
+  front.style.backgroundImage = `url('/imagens/${character}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
 
   card.addEventListener('click', revealCard);
-  card.setAttribute('data-character', character)
+  card.setAttribute('data-character', character);
 
   return card;
 }
 
 const loadGame = () => {
   const duplicateCharacters = [...characters, ...characters];
-
   const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
 
   shuffledArray.forEach((character) => {
@@ -111,12 +100,10 @@ const loadGame = () => {
 }
 
 const startTimer = () => {
-
   this.loop = setInterval(() => {
     const currentTime = +timer.innerHTML;
     timer.innerHTML = currentTime + 1;
   }, 1000);
-
 }
 
 window.onload = () => {
