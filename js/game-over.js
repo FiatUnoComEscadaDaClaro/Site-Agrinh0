1574a84efc73cfd730f05ea33bed46bbdf83f4ee
FiatUnoComEscadaDaClaro <127310216+FiatUnoComEscadaDaClaro@users.noreply.github.com>
2024-08-08 18:51:57 -0300

    Update game-over.js

--- js/game-over.js
+++ js/game-over.js
@@ -1,16 +1,8 @@
 window.onload = () => {
   const finalTimeElement = document.getElementById('final-time');
-  const recordTimeElement = document.getElementById('record-time');
 
-  // Obter o tempo final e o recorde da localStorage
-  const finalTime = localStorage.getItem('finalTime');
-  const recordTime = localStorage.getItem('record') || 'Nenhum';
+  // Obter o tempo final da localStorage
+  const finalTime = localStorage.getItem('finalTime') || '0';
 
   finalTimeElement.textContent = finalTime;
-  recordTimeElement.textContent = recordTime;
-
-  // Atualizar o recorde se necessário
-  if (finalTime && (recordTime === 'Nenhum' || parseInt(finalTime) < parseInt(recordTime))) {
-    localStorage.setItem('record', finalTime);
-  }
 }
